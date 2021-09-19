ad2cf7b5cdd79700193b33da98ddfedfc710a449
DESKTOP-PNQ3FLQ\jahhy <royalh69@gmail.com>
2021-09-19 13:24:07 +0900

    move block to left or right depends on cursor position testing

--- js/tetris.js
+++ js/tetris.js
@@ -53,7 +53,7 @@ const colors = [
 
 let item = new Block(colors[0])
 
-// Object : Piece
+// Object : Block
 function Block(color) {
     this.block = block;
     this.color = color;
@@ -73,38 +73,49 @@ Block.prototype.fill = function(color) {
     }
 }
 
-// draw a piece to the board
+// draw a block to the board
 Block.prototype.draw = function() {
     this.fill(this.color);
-    // for (r = 0; r < this.block.length; r++) {
-    //     for (c = 0; c < this.block.length; c++) {
-    //         if (this.block[r][c]) {
-    //             drawCell(this.x + c, this.y + r, this.color);
-    //         }
-    //     }
-    // }
 }
 
-// undraw a piece
+// undraw a block
 Block.prototype.undraw = function() {
     this.fill(VACANT)
-    // for (r = 0; r < this.block.length; r++) {
-    //     for (c = 0; c < this.block.length; c++) {
-    //         if (this.block[r][c]) {
-    //             drawCell(this.x + c, this.y + r, VACANT);
-    //         }
-    //     }
-    // }
 }
 
-// move down the piece
+// move down the block
 Block.prototype.moveDown = function() {
     this.undraw();
     this.y++;
     this.draw();
 }
 
-// drop the piece every 1sec
+// move left the block
+Block.prototype.moveLeft = function() {
+    this.undraw();
+    this.x--;
+    this.draw();
+}
+
+// move right the block
+Block.prototype.moveRight = function() {
+    this.undraw();
+    this.x++;
+    this.draw();
+}
+
+// control the block
+document.addEventListener('mouseover', CONTROL);
+function CONTROL(event) {
+    if (event.clientX < item.x * 20) {
+        console.log(event.clientX);
+        item.moveLeft();
+    } else if (event.clientX > item.x * 20) {
+        item.moveRight();
+    }
+}
+
+// drop the block every 1sec
 let dropStart = Date.now();
 function drop() {
     let now = Date.now();
