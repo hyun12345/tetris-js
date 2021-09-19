ba292071099edc7e8bc23841801752d474da5dab
DESKTOP-PNQ3FLQ\jahhy <royalh69@gmail.com>
2021-09-19 13:49:03 +0900

    set new block testing

--- js/tetris.js
+++ js/tetris.js
@@ -1,8 +1,8 @@
 const cvs = document.getElementById('tetris');
 const ctx = cvs.getContext('2d');
 
-const row = 20;
-const col = 10;
+const row = 10;
+const col = 20;
 const cell = 20;
 const VACANT = 'white';
 
@@ -23,18 +23,18 @@ function drawCell(x, y, color) {
 
 // create the board
 let board = [];
-for (r = 0; r < row; r++) {
-    board[r] = [];
-    for (c = 0; c < col; c++) {
-        board[r][c] = VACANT;
+for (c = 0; c < col; c++) {
+    board[c] = [];
+    for (r = 0; r < row; r++) {
+        board[c][r] = VACANT;
     }
 }
 
 // draw the board
 function drawBoard() {
-    for (r = 0; r < row; r++) {
-        for (c = 0; c < col; c++) {
-            drawCell(c, r, board[r][c]);
+    for (c = 0; c < col; c++) {
+        for (r = 0; r < row; r++) {
+            drawCell(r, c, board[c][r]);
         }
     }
 }
@@ -51,7 +51,13 @@ const colors = [
     ['purple']
 ]
 
-let item = new Block(colors[0])
+// generate blocks
+function setBlock() {
+    let random = Math.floor(Math.random() * colors.length)
+    return new Block(colors[random][0])
+}
+
+let item = setBlock();
 
 // Object : Block
 function Block(color) {
@@ -64,10 +70,10 @@ function Block(color) {
 
 // fill function
 Block.prototype.fill = function(color) {
-    for (r = 0; r < this.block.length; r++) {
-        for (c = 0; c < this.block.length; c++) {
-            if (this.block[r][c]) {
-                drawCell(this.x + c, this.y + r, color);
+    for (c = 0; c < this.block.length; c++) {
+        for (r = 0; r < this.block.length; r++) {
+            if (this.block[c][r]) {
+                drawCell(this.x + r, this.y + c, color);
             }
         }
     }
@@ -85,32 +91,66 @@ Block.prototype.undraw = function() {
 
 // move down the block
 Block.prototype.moveDown = function() {
-    this.undraw();
-    this.y++;
-    this.draw();
+    if (!this.collision(0, 1, this.block)) {
+        this.undraw();
+        this.y++;
+        this.draw();
+    } else {
+        item = setBlock();
+    }
 }
 
 // move left the block
 Block.prototype.moveLeft = function() {
-    this.undraw();
-    this.x--;
-    this.draw();
+    if (!this.collision(-1, 0, this.block)) {
+        this.undraw();
+        this.x--;
+        this.draw();
+    }
 }
 
 // move right the block
 Block.prototype.moveRight = function() {
-    this.undraw();
-    this.x++;
-    this.draw();
+    if (!this.collision(1, 0, this.block)) {
+        this.undraw();
+        this.x++;
+        this.draw();
+    }
+}
+
+// colision function
+Block.prototype.collision = function(x, y, block) {
+    for (c = 0; c < block.length; c++) {
+        for (r = 0; r < block.length; r++) {
+            // skip when cell is empty
+            if(!block[c][r]) {
+                continue;
+            }
+            let newX = this.x + r + x;
+            let newY = this.y + c + y;
+
+            if (newX < 0 || newX >= row || newY >= col) {
+                return true;
+            }
+
+            if (newY < 0) {
+                continue;
+            }
+            if (board[newY][newX] != VACANT) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 // control the block
 document.addEventListener('mouseover', CONTROL);
 function CONTROL(event) {
-    if (event.clientX < item.x * 20) {
+    if (event.clientX < (item.x * 20) + 40) {
         console.log(event.clientX);
         item.moveLeft();
-    } else if (event.clientX > item.x * 20) {
+    } else if (event.clientX > (item.x * 20) - 20) {
         item.moveRight();
     }
 }
