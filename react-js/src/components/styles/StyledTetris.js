d6529b2afd3e66ee79f4e63e51444f95e30318d0
DESKTOP-PNQ3FLQ\jahhy <royalh69@gmail.com>
2021-09-20 11:57:34 +0900

    *update Canvas.js : using id onMouseMove property *update StyledTetris.js : change background-color *update Tetris.js 1) 사용하지 않는 코드 주석처리(keyUp, keyDown) 2) use getBoundingClientRect(), getElemenyById() to get canvas width and offsetWidth (because it's responsive web component) 3) get calculated cellSize

--- react-js/src/components/styles/StyledTetris.js
+++ react-js/src/components/styles/StyledTetris.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledTetrisWrapper = styled.div`
     width: 100vw;
     height: 100vh;
-    background-color: gray;
+    background-color: black;
     overflow: hidden;
 `
 
